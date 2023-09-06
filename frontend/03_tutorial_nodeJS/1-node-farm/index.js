072b7e5a2c515d65948b3ee7676b8a6048fca519
Jiyu Guo <jiyu@192.168.0.100>
2023-09-06 19:35:08 +0800

    a

---
+++ frontend/03_tutorial_nodeJS/1-node-farm/index.js
@@ -0,0 +1,102 @@
+const fs = require('fs');
+const http = require('http');
+const url = require('url');
+/////////////////////////////////////////////////////////////////////////////////
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
+// for (let i = 0; i < 10; i++) {
+//     console.log(textIn);
+// }
+// const textOut = `This is what we know about the avocado:${textIn}.\nCreated on ${Date.now()}`
+// fs.writeFileSync('./txt/output.txt',textOut);
+// console.log('File written');
+
+/////////////////////////////////////////////////////////////////////////////////
+//Non-blocking, asynchronous way
+//callback hell example -->
+// fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
+//
+//     if(err) return console.log('ERROR!');
+//
+//     fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2)=>{
+//         console.log(data2);
+//
+//         fs.readFile('./txt/append.txt','utf-8',(err,data3)=>{
+//             console.log(data3);
+//
+//             fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,(err)=>{
+//                 console.log('Your file has been written');
+//             });
+//         });
+//     });
+// });
+// console.log('Will read file!');
+/////////////////////////////////////////////////////////////////////////////////
+//Server
+const replaceTemplate = (temp,product) =>{
+    let output = temp.replace(/{%PRODUCTNAME%}/g,product.productName);
+    output = output.replace(/{%IMAGE%}/g,product.image);
+    output = output.replace(/{%PRICE%}/g,product.price);
+    output = output.replace(/{%FROM%}/g,product.from);
+    output = output.replace(/{%NUTRIENTS%}/g,product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g,product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g,product.description);
+    output = output.replace(/{%ID%}/g,product.id);
+
+    if(!product.organic) output = output.replace('/{%NOT_ORGANIC%}/g','not-organic');
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`./templates/template-overview.html`,'utf-8')
+const tempCard = fs.readFileSync(`./templates/template-card.html`,'utf-8')
+const tempProduct = fs.readFileSync(`./templates/template-product.html`,'utf-8')
+
+const data = fs.readFileSync(`./dev-data/data.json`,'utf-8')
+const dataObj = JSON.parse(data);
+
+
+
+
+const server = http.createServer((req,res)=>{
+
+    const {query,pathname} = url.parse(req.url,true);
+
+    //Overview
+    if(pathName === '/overview'){
+        res.writeHead(200,{
+            'Content-type':'html'
+        });
+
+        const cardsHtml = dataObj.map(ele => replaceTemplate(tempCard, ele)).join('');
+        const output = tempOverview.replace('{%PRODUCT_CARDS%}',cardsHtml)
+
+        res.end(output);
+    }
+
+    //Product
+    else if(pathName === '/product'){
+        console.log(query)
+        res.end('This is the PRODUCT');
+    }
+    //API
+    else if(pathName === '/api'){
+        res.writeHead(200,{
+            'Content-type':'application/json'
+        })
+        res.end(data);
+    }
+    //Not Found
+    else {
+        res.writeHead(404,{
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        });
+        res.end('<h1>Page not found!</h1>');
+    }
+
+    // res.end('Hello from the server!');
+});
+
+server.listen(8000,'127.0.0.1',()=>{
+    console.log('Listening to requests on port 8000');
+})
