32dc8e80a9bd137f02553859a221f1dfa162c325
Jiyu Guo <jiyu@192.168.0.100>
2023-09-09 13:10:10 +0800

    a

--- frontend/03_tutorial_nodeJS/01_Buffer/bufferCreation.js
+++ frontend/03_tutorial_nodeJS/01_Buffer/bufferCreation.js
@@ -1,13 +1,11 @@
-//1.alloc
+//1. alloc
 let buf = Buffer.alloc(10);
-console.log(buf);
-
-//2.allocUnsafe
+console.log(buf)
+//2. allocUnsafe
 let buf2 = Buffer.allocUnsafe(10);
 console.log(buf2);
-
-//3.from
-let buf3 = Buffer.from('Hello'); //Unicode
-let buf4 = Buffer.from([1,2,3,4,5]);
+//3. from
+let buf3 = Buffer.from('Hello');
 console.log(buf3);
+let buf4 = Buffer.from([101,102,103,104,105,106]);
 console.log(buf4);
